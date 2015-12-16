d25e15a82a795fda645f6e3c514f1f1ca3f97d82
Ryan Borek <ryanborek.ca@gmail.com>
2015-12-15 19:25:11 -0800

    changed to make more sense

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -7,12 +7,12 @@ int GetLowestIntForNZeroes(string secret_key, int n) {
 	string hash;
 	for (int i = 0; i < INT_MAX; ++i){
 		hash = md5(secret_key + to_string(i));
-		for(int j = 0; j < n + 1; ++j) {
+		for(int j = 0; j < n; ++j) {
 			if (hash[j] != '0') {
-				if (j == n) {
-					return i;
-				}
 				break;
+			}
+			if (j == n - 1) {
+				return i;
 			}
 		}
 	}
@@ -24,4 +24,3 @@ int main() {
 	cout << GetLowestIntForNZeroes(secret_key, 5) << endl;
 	cout << GetLowestIntForNZeroes(secret_key, 6) << endl;
 }
-
